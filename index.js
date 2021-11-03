95f31e4595994c5a6fcc025be13a657493052fd8
Antonio Ruben Paitno <antonio.patino003@gmail.com>
2021-11-03 12:21:36 -0500

    commit tree dae9d5ab87d1e86d380930af54fd214f8d3634db

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+  
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+let num3 = 8;
+let num4 = 2;
+
+const mod= num3/num4
+
+let max = Math.max (1,2,3,20)
+
+
